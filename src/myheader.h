b36a4f18c30e01838b6424d3ca2b9a4d77ca8a9d
Mahan MI <majlesi.mahan@gmail.com>
2023-12-30 21:40:52 +0330

    add myheader.h

---
+++ src/myheader.h
@@ -0,0 +1,12 @@
+// Add your own header files here
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include <SDL2_ttf/SDL_ttf.h>
+#include <SDL2_gfx/SDL2_framerate.h>
+#include <SDL2_gfx/SDL2_gfxPrimitives.h>
+#include <SDL2_gfx/SDL2_imageFilter.h>
+#include <SDL2_gfx/SDL2_rotozoom.h>
+#include <string>
+
+using namespace std;
