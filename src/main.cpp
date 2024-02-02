12c6b1080ad9497fbd7ff0935b206f76b6465cde
Mahan MI <majlesi.mahan@gmail.com>
2024-02-02 21:02:22 +0330

    inputbox implemented (Not Complete)

--- src/main.cpp
+++ src/main.cpp
@@ -131,6 +131,7 @@ int main(int argc, char const *argv[])
         button classicGM = {142, 270, 243, 432, 0, 0, 0, 0, 0, true, false, IMG_LoadTexture(renderer, "assets/GameMode/Classic.png"), IMG_LoadTexture(renderer, "assets/GameMode/ClassicHover.png"), IMG_LoadTexture(renderer, "assets/GameMode/classicSelected.png"), hover, click, true};
         button infiniteGM = {142, 270, 422, 432, 0, 0, 0, 0, 0, false, false, IMG_LoadTexture(renderer, "assets/GameMode/Infinite.png"), IMG_LoadTexture(renderer, "assets/GameMode/InfiniteHover.png"), IMG_LoadTexture(renderer, "assets/GameMode/InfiniteSelected.png"), hover, click, false};
         button start = {198, 91, 219, 829, 0, 0, 0, 0, 5, false, false, IMG_LoadTexture(renderer, "assets/GameMode/startButton.png"), IMG_LoadTexture(renderer, "assets/GameMode/startButtonHover.png"), IMG_LoadTexture(renderer, "assets/GameMode/startButtonClick.png"), hover, click};
+        button inputBox = {319, 38, 236, 296, 0, 0, 0, 0, 0, false, false, IMG_LoadTexture(renderer, "assets/GameMode/inputBox.png"), IMG_LoadTexture(renderer, "assets/GameMode/inputBoxHover.png"), IMG_LoadTexture(renderer, "assets/GameMode/infiniteSelected.png"), hover, click};
         while (play.wasClicked)
         {
           while (SDL_PollEvent(&event) && play.wasClicked)
@@ -189,6 +190,36 @@ int main(int argc, char const *argv[])
             }
 
             SDL_RenderCopy(renderer, PlayBG, 0, 0);
+
+            drawButton(renderer, inputBox, event);
+
+            if (inputBox.wasClicked)
+            {
+              string name = "";
+              while (inputBox.wasClicked)
+              {
+                while (SDL_PollEvent(&event) && inputBox.wasClicked)
+                {
+                  if (SDL_KEYDOWN)
+                  {
+                    switch (event.key.keysym.sym)
+                    {
+                    case SDLK_SPACE:
+                      cout << name << endl;
+                      inputBox.wasClicked = false;
+                      break;
+                    case SDLK_BACKSPACE:
+                      if (name.size() > 0)
+                        name.pop_back();
+                      break;
+                    default:
+                      name += event.key.keysym.sym;
+                      break;
+                    }
+                  }
+                }
+              }
+            }
 
             drawButton(renderer, timerGM, event);
             drawButton(renderer, classicGM, event);
